29a6b4d331b1f9efd0eeb9f2db8c1bebc9b510c1
Leila Borromeo <leilaborromeo96@gmail.com>
2023-05-26 11:28:57 +0800

    Add project files.

---
+++ Activity2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// for experimentation: what if i used enums or something to make a more structured code for the switch cases? then based on that enum, every case in switch statement would be referring to some kind of template in C#
+// Note: actual namespace depends on the project name.
+namespace ADET_activities {  
+    public class Program {
+        static bool validateArg(int arg) { 
+            return ((arg > 3) && (arg % 2) != 0) ? true : false;
+        }
+
+        public static void Main(string[] args) {
+            int choice = 0;
+            int arg = 0;
+
+            do {
+                Console.WriteLine("Shape Drawing");
+                Console.WriteLine("Choose a number between 1 and 13 (inclusive):");
+                Console.WriteLine("1 - Parallelogram");
+                Console.WriteLine("2 - Triangle");
+                Console.WriteLine("3 - Reverse Triangle");
+                Console.WriteLine("4 - Isosceles Triangle");
+                Console.WriteLine("5 - Reverse Isosceles Triangle");
+                Console.WriteLine("6 - Hourglass");
+                Console.WriteLine("7 - Diamond");
+                Console.WriteLine("8 - Zero");
+                Console.WriteLine("9 - Arrow Up");
+                Console.WriteLine("10 - Arrow Down");
+                Console.WriteLine("11 - X");
+                Console.WriteLine("12 - Bow Tie");
+                Console.WriteLine("13 - EXIT PROGRAM");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice) {
+                    case 1:                   
+                        do {
+                            Console.WriteLine("Enter the total number of lines for your PARALLELOGRAM (odd and must be greater than 3)");
+                            arg = Convert.ToInt32(Console.ReadLine());
+
+                            if (validateArg(arg)) {
+                                act_two_functions.drawParallelogram(arg);
+                            }
+                            else {
+                                Console.WriteLine("Invalid input.");
+                            }
+                        } while(!validateArg(arg));
+                        break;
+                    case 2:
+                        do {
+                            Console.WriteLine("Enter the total number of lines for your TRIANGLE (odd and must be greater than 3)");
+                            arg = Convert.ToInt32(Console.ReadLine());
+
+                            if(validateArg(arg)) {
+                                act_two_functions.drawTriangle(arg);
+                            }
+                            else {
+                                Console.WriteLine("Invalid input.");
+                            }
+                        } while(!validateArg(arg));
+                        break;
+                    case 3:
+                        do {
+                            Console.WriteLine("Enter the total number of lines for your REVERSE TRIANGLE (odd and must be greater than 3)");
+                            arg = Convert.ToInt32(Console.ReadLine());
+
+                            if(validateArg(arg)) {
+                                act_two_functions.drawReverseTriangle(arg);
+                            }
+                            else {
+                                Console.WriteLine("Invalid input.");
+                            }
+                        } while(!validateArg(arg));
+                        break;
+                    case 4:
+                        do {
+                            Console.WriteLine("Enter the total number of asterisks as the base for your ISOSCELES TRIANGLE (odd and must be greater than 3)");
+                            arg = Convert.ToInt32(Console.ReadLine());
+
+                            if (validateArg(arg)) {
+                                act_two_functions.drawIsoscelesTriangle(arg);
+                            }
+                            else {
+                                Console.WriteLine("Invalid input.");
+                            }
+                        } while(!validateArg(arg));
+                        break;
+                    case 5:
+                        do {
+                            Console.WriteLine("Enter the total number of asterisks as the top for your REVERSED ISOSCELES TRIANGLE (odd and must be greater than 3)");
+                            arg = Convert.ToInt32(Console.ReadLine());
+
+                            if (validateArg(arg)) {
+                                act_two_functions.drawReversedIsoscelesTriangle(arg);
+                            }
+                            else {
+                                Console.WriteLine("Invalid input.");
+                            }
+                        } while(!validateArg(arg));
+                        break;
+                    case 13:
+                        Console.WriteLine("Program Terminated.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+
+                }
+
+            } while(choice >= 1 && choice <= 12);
+        }
+    }
+}
---
+++ act_two_functions.cs
@@ -0,0 +1,74 @@
+﻿namespace ADET_activities {
+    public class act_two_functions {
+        public static void drawParallelogram(int number) {
+            for(int row = 0; row < number; row++) {
+                for(int bef = 0; bef < number - row - 1; bef++) {
+                    Console.Write("_");
+                }
+                
+                for(int ast = 0; ast < number; ast++) {
+                    Console.Write("*");
+                }
+
+                for(int aft = 0; aft < row; aft++) {
+                    Console.Write("_");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void drawTriangle(int number) {
+            for (int i = 0; i < number; i++) {
+                for (int j = 1; j <= number - i; j++) {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j <= i; j++) {
+                    Console.Write("_");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void drawReverseTriangle(int number) {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= i; j++) {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j <= number - i; j++) {
+                    Console.Write("_");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void drawIsoscelesTriangle(int number) {
+            int space = number / 2;
+
+            for (int r = 1; r <= (number / 2) + 1; r++) {
+                for (int sp = 1; sp <= space; sp++) { // number - r
+                    Console.Write("_");
+                }
+
+                for (int c = 1; c <= (r * 2) - 1; c++) {
+                    Console.Write("*");
+                }
+
+                for (int sp = 1; sp <= space; sp++) { // number - r
+                    Console.Write("_");
+                }
+
+                space--;
+                Console.WriteLine();
+            }
+        }
+
+        public static void drawReversedIsoscelesTriangle(int number) {
+
+        }
+    }
+}
